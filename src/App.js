5539454c27abe32ad70b24907debcaac6ef2d4a7
onth0s <metaagnosis@gmail.com>
2022-03-03 03:23:34 +0100

    lalala

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
+import words from './resources/all_words.json';
+
 function App() {
+	const [numWords, setNumWords] = useState(500);
+	const loadMoreWords = () => setNumWords(numWords + 500);
+
+	useEffect(() => {
+		window.onscroll = (e) => {
+			if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+				loadMoreWords();
+			}
+		};
+	}, [loadMoreWords]);
+
+	const offset = 10;
+
+	
+
 	return (
-		<div className="container bg-blue-200">
-			Completely clean and empty
-			<span className="mx-1 italic">create-react-app</span>
-			template.
+		<div className="overflow-hidden bg-black" >
+			<div className="flex flex-wrap justify-center space-x-1 space-y-1"
+				style={{ width: `calc(100vw + ${offset}vw)`, marginLeft: `-${offset / 2}vw` }}
+			>
+				{words.map((el, i) => (
+					(i < numWords) && <div className="px-2 w-max border-black rounded-full" key={i}
+						style={{
+							color: `hsl(0, 0%, ${Math.cos(i * 0.002) * 100}%)`,
+							backgroundColor: `hsl(${(i / 10) - 50}, 100%, 50%)`,
+						}}
+					>{el}</div>
+				))}
+			</div>
 		</div>
 	);
 }
